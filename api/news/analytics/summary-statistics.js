662f253c924fe06bbc867f4a01c0fe68b23235b9
Xiaoxi <yxx0827@gmail.com>
2020-03-22 00:12:06 -0400

    add word cloud

--- api/news/analytics/summary-statistics.js
+++ api/news/analytics/summary-statistics.js
@@ -2,6 +2,7 @@ const getDocumentsCount = require('../analytics/reusables/get-documents-count').
 const getDocumentDate = require('../analytics/reusables/get-earliest-and-latest-document-date').getDocumentDate;
 const getFrequencyAnalytics = require('./frequency-by-day').getFrequencyAnalytics;
 const getDocumentsCountByCategory = require('./reusables/get-documents-count-by-category').getDocumentsCountByCategory;
+const getWordCloud = require('./word-cloud').getWordCloud;
 
 let cache = null;
 
@@ -18,13 +19,15 @@ function getSummaryStatistics(newsCollection) {
             const latestDocumentDate = await getDocumentDate('latest', newsCollection);
             const documentsCountByDay = await getFrequencyAnalytics('', newsCollection);
             const documentsCountByCategory = await getDocumentsCountByCategory(newsCollection);
+            const wordCloud = await getWordCloud(newsCollection);
 
             cache = {
                 totalDocuments,
                 earliestDocumentDate,
                 latestDocumentDate,
                 documentsCountByDay,
-                documentsCountByCategory
+                documentsCountByCategory,
+                wordCloud
             };
 
             resolve(cache)
---
+++ api/news/analytics/word-cloud.js
@@ -0,0 +1,109 @@
+let cache = null;
+
+const media = [
+    "global", "cbc.ca", "ctv", "sportsnet.ca", "cnn", "tsn", "showbiz", "cheat", "sheet",
+    "business", "insider"
+];
+const blacklist = [
+    ...media,
+    "null", "news", "new", "first", "now",
+    "one", "two",
+    "just", "announced", "may", "according", "time", "get",
+    "", "also", "i", "me", "my", "myself", "we", "us",
+    "our", "ours", "ourselves", "you", "your", "yours",
+    "yourself", "yourselves", "he", "him", "his",
+    "himself", "she", "her", "hers", "herself", "it",
+    "its", "itself", "they", "them", "their", "theirs",
+    "themselves", "what", "which", "who", "whom", "whose",
+    "this", "that", "these", "those", "am", "is", "are",
+    "was", "were", "be", "been", "being", "have", "has",
+    "had", "having", "do", "does", "did", "doing", "will",
+    "would", "should", "can", "could", "ought", "i'm",
+    "you're", "he's", "she's", "it's", "we're", "they're",
+    "i've", "you've", "we've", "they've", "i'd", "you'd",
+    "he'd", "she'd", "we'd", "they'd", "i'll", "you'll",
+    "he'll", "she'll", "we'll", "they'll", "isn't",
+    "aren't", "wasn't", "weren't", "hasn't", "haven't",
+    "hadn't", "doesn't", "don't", "didn't", "won't",
+    "wouldn't", "shan't", "shouldn't", "can't", "cannot",
+    "couldn't", "mustn't", "let's", "that's", "who's",
+    "what's", "here's", "there's", "when's", "where's",
+    "why's", "how's", "a", "an", "the", "and", "but",
+    "if", "or", "because", "as", "until", "while", "of",
+    "at", "by", "for", "with", "about", "against",
+    "between", "into", "through", "during", "before",
+    "after", "above", "below", "to", "from", "up", "upon",
+    "down", "in", "out", "on", "off", "over", "under",
+    "again", "further", "then", "once", "here", "there", "when",
+    "where", "why", "how", "all", "any", "both", "each",
+    "few", "more", "most", "other", "some", "such", "no",
+    "nor", "not", "only", "own", "same", "so", "than",
+    "too", "very", "say", "says", "said", "shall"];
+
+function getWordCloud(collection) {
+    return new Promise((resolve, reject) => {
+        if (cache) resolve(cache);
+
+        const pipeline = [
+            {
+                $addFields: {
+                    words: {
+                        $map: {
+                            input: { $split: ['$description', ' '] },
+                            as: 'str',
+                            in: {$cond: {
+                                if: {
+                                    $not: [{
+                                        $in: [
+                                            "$$str",
+                                            [",", "|", "(", ")", "{", "}", "—", "<", ">", ".", ";", "\"", "&", "...", "…"]
+                                        ]
+                                    }]
+                                },
+                                then: {
+                                    $toLower: "$$str"
+                                },
+                                else: "null"
+                            }}
+                        }
+                    }
+                }
+            },
+            { $unwind: '$words' },
+            {
+                $match: {
+                    words: {
+                        $nin: blacklist
+                    }
+                }
+            },
+            {
+                $group: {
+                    _id: {
+                        word: "$words"
+                    },
+                    count: {
+                        $sum: 1
+                    }
+                }
+            },
+            {$sort: {count: -1}},
+            {$limit: 20},
+            {$project: {
+                word: "$_id.word",
+                count: 1,
+                _id: 0
+                }}
+        ];
+        collection.aggregate(pipeline).toArray((err, doc) => {
+            if (err) reject(err);
+            console.log(doc);
+            cache = doc;
+            resolve(doc);
+        })
+    })
+}
+
+module.exports = {
+    getWordCloud
+};
